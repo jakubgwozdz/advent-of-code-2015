fe6f31e31f070a329a1fdc2fd3c0cfca5ee357e7
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2022-11-25 17:39:14 +0100

    day 4

--- src/main/kotlin/day4/Day4.kt
+++ src/main/kotlin/day4/Day4.kt
@@ -2,20 +2,21 @@ package day4
 
 import java.security.MessageDigest
 
-fun part1(input: String): Long {
-    var l = 1L
-    while (!md5("${input.trim()}$l").let { it[0].toInt() == 0 && it[1].toInt() == 0 && it[2].toInt() in (0..15) }) l++
-    return l
-}
+const val b0 = 0.toByte()
+const val bF = 15.toByte()
 
-fun md5(str: String) = MessageDigest.getInstance("MD5")
-    .digest(str.toByteArray())
+private val fiveZeroes: ByteArray.() -> Boolean = { this[0] == b0 && this[1] == b0 && this[2] in b0..bF }
+private val sixZeroes: ByteArray.() -> Boolean = { this[0] == b0 && this[1] == b0 && this[2] == b0 }
 
-fun part2(input: String): Long {
-    var l = 1L
-    while (!md5("${input.trim()}$l").let { it[0].toInt() == 0 && it[1].toInt() == 0 && it[2].toInt() == 0 }) l++
-    return l
-}
+fun part1(input: String) = generateSequence(1L) { it + 1 }
+    .takeWhile { !md5("${input.trim()}$it").fiveZeroes() }
+    .last() + 1
+
+private fun md5(str: String): ByteArray = MessageDigest.getInstance("MD5").digest(str.toByteArray())
+
+fun part2(input: String) = generateSequence(1L) { it + 1 }
+    .takeWhile { !md5("${input.trim()}$it").sixZeroes() }
+    .last() + 1
 
 fun main() {
     println(part1("abcdef"))
