36a9063542b1a80c4d48e9382ea952008af22f79
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2022-11-27 19:56:19 +0100

    day 7

--- src/main/kotlin/day7/Day7.kt
+++ src/main/kotlin/day7/Day7.kt
@@ -7,70 +7,33 @@ fun main() {
     println(part2(readAllText("local/day7_input.txt")))
 }
 
-sealed interface Op {
-    fun value(circuit: Map<String, Op>): Int
-    data class Signal(val v: Int) : Op {
-        override fun value(circuit: Map<String, Op>): Int = v
-    }
+private fun Map<String, Op>.calc(wire: String) = wire.toIntOrNull() ?: this[wire]!!.value(this)
 
-    data class Short(val w1: String) : Op {
-        private var cached: Int? = null
-        override fun value(circuit: Map<String, Op>): Int = cached
-            ?: circuit[w1]!!.value(circuit)
-                .also { cached = it }
-    }
+sealed class Op(val op: Map<String, Op>.() -> Int) {
+    var cached: Int? = null
+    fun value(circuit: Map<String, Op>): Int = cached ?: op(circuit).also { cached = it }
 
-    data class Or(val w1: String, val w2: String) : Op {
-        private var cached: Int? = null
-        override fun value(circuit: Map<String, Op>): Int = cached
-            ?: (circuit[w1]!!.value(circuit) or circuit[w2]!!.value(circuit))
-                .also { cached = it }
-    }
-
-    data class Not(val w1: String) : Op {
-        private var cached: Int? = null
-        override fun value(circuit: Map<String, Op>): Int = cached
-            ?: (circuit[w1]!!.value(circuit).inv() and 0xffff)
-                .also { cached = it }
-    }
-
-    data class And(val w1: String, val w2: String) : Op {
-        private var cached: Int? = null
-        override fun value(circuit: Map<String, Op>): Int = cached
-            ?: (circuit[w1]!!.value(circuit) and circuit[w2]!!.value(circuit))
-                .also { cached = it }
-    }
-
-    data class LShift(val w1: String, val sh: Int) : Op {
-        private var cached: Int? = null
-        override fun value(circuit: Map<String, Op>): Int = cached
-            ?: (circuit[w1]!!.value(circuit) shl sh and 0xffff)
-                .also { cached = it }
-    }
-
-    data class RShift(val w1: String, val sh: Int) : Op {
-        private var cached: Int? = null
-        override fun value(circuit: Map<String, Op>): Int = cached
-            ?: (circuit[w1]!!.value(circuit) ushr sh and 0xffff)
-                .also { cached = it }
-    }
+    data class Short(val w1: String) : Op({ calc(w1) })
+    data class Or(val w1: String, val w2: String) : Op({ calc(w1) or calc(w2) })
+    data class And(val w1: String, val w2: String) : Op({ calc(w1) and calc(w2) })
+    data class Not(val w1: String) : Op({ calc(w1).inv() and 0xffff })
+    data class LShift(val w1: String, val sh: Int) : Op({ calc(w1) shl sh and 0xffff })
+    data class RShift(val w1: String, val sh: Int) : Op({ calc(w1) ushr sh and 0xffff })
 }
 
 
 private fun parse(line: String) =
-    "(\\d+) -> (\\w+)".toRegex().matchEntire(line)?.destructured
-        ?.let { (v, wire) -> wire to Op.Signal(v.toInt()) }
-        ?: "(\\w+) -> (\\w+)".toRegex().matchEntire(line)?.destructured
-            ?.let { (w1, wire) -> wire to Op.Short(w1) }
-        ?: "(\\w+) OR (\\w+) -> (\\w+)".toRegex().matchEntire(line)?.destructured
+    "(\\w+) -> ([a-z]+)".toRegex().matchEntire(line)?.destructured
+        ?.let { (w1, wire) -> wire to Op.Short(w1) }
+        ?: "(\\w+) OR (\\w+) -> ([a-z]+)".toRegex().matchEntire(line)?.destructured
             ?.let { (w1, w2, wire) -> wire to Op.Or(w1, w2) }
-        ?: "(\\w+) AND (\\w+) -> (\\w+)".toRegex().matchEntire(line)?.destructured
+        ?: "(\\w+) AND (\\w+) -> ([a-z]+)".toRegex().matchEntire(line)?.destructured
             ?.let { (w1, w2, wire) -> wire to Op.And(w1, w2) }
-        ?: "NOT (\\w+) -> (\\w+)".toRegex().matchEntire(line)?.destructured
+        ?: "NOT (\\w+) -> ([a-z]+)".toRegex().matchEntire(line)?.destructured
             ?.let { (w1, wire) -> wire to Op.Not(w1) }
-        ?: "(\\w+) LSHIFT (\\d+) -> (\\w+)".toRegex().matchEntire(line)?.destructured
+        ?: "(\\w+) LSHIFT (\\d+) -> ([a-z]+)".toRegex().matchEntire(line)?.destructured
             ?.let { (w1, sh, wire) -> wire to Op.LShift(w1, sh.toInt()) }
-        ?: "(\\w+) RSHIFT (\\d+) -> (\\w+)".toRegex().matchEntire(line)?.destructured
+        ?: "(\\w+) RSHIFT (\\d+) -> ([a-z]+)".toRegex().matchEntire(line)?.destructured
             ?.let { (w1, sh, wire) -> wire to Op.RShift(w1, sh.toInt()) }
         ?: error("Wtf `$line`")
 
@@ -81,5 +44,12 @@ fun part1(input: String): Int {
     return circuit["a"]!!.value(circuit)
 }
 
-fun part2(input: String) = input.lineSequence().filterNot(String::isBlank)
-    .count()
+fun part2(input: String): Int {
+    val circuit = input.lineSequence().filterNot(String::isBlank)
+        .map { parse(it) }
+        .toMap().toMutableMap()
+    val a = circuit["a"]!!.value(circuit)
+    circuit["b"] = Op.Short(a.toString())
+    circuit.forEach { (k, v) -> v.cached = null }
+    return circuit["a"]!!.value(circuit)
+}
