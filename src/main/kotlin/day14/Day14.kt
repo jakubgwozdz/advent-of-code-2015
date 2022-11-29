0f16efb9e5a44c849d613aa92d9b60f6f12c458a
Jakub Gwóźdź <jakub.gwozdz@schibsted.com>
2022-11-29 20:42:50 +0100

    day 14,15

--- src/main/kotlin/day14/Day14.kt
+++ src/main/kotlin/day14/Day14.kt
@@ -7,8 +7,32 @@ fun main() {
     println(part2(readAllText("local/day14_input.txt")))
 }
 
-fun part1(input: String) = input.lineSequence().filterNot(String::isBlank)
-    .count()
+data class Reinder(val name: String, val speed: Int, val time: Int, val rest: Int)
 
-fun part2(input: String) = input.lineSequence().filterNot(String::isBlank)
-    .count()
+private val regex = "(\\w+) can fly (\\d+) km/s for (\\d+) seconds, but then must rest for (\\d+) seconds.".toRegex()
+
+fun part1(input: String) = parse(input)
+    .maxOf { r ->
+        distance(r, 2503)
+    }
+
+fun part2(input: String): Int {
+    val reinders = parse(input).toList()
+    val score = reinders.associate { it.name to 0 }.toMutableMap()
+    (1..2503).forEach { elapsed ->
+        reinders.groupBy { distance(it, elapsed) }.maxBy { (k, v) -> k }.value
+            .forEach { score[it.name] = score[it.name]!! + 1 }
+    }
+
+    return score.maxOf { it.value }
+}
+
+private fun distance(r: Reinder, elapsed: Int): Int {
+    val cycles = elapsed / (r.time + r.rest)
+    val remainder = elapsed % (r.time + r.rest)
+    return cycles * r.speed * r.time + minOf(r.time, remainder) * r.speed
+}
+
+private fun parse(input: String) = input.lineSequence().filterNot(String::isBlank)
+    .map { line -> regex.matchEntire(line)?.destructured ?: error("WTF `$line`") }
+    .map { (a, b, c, d) -> Reinder(a, b.toInt(), c.toInt(), d.toInt()) }
--- src/main/kotlin/day15/Day15.kt
+++ src/main/kotlin/day15/Day15.kt
@@ -7,8 +7,48 @@ fun main() {
     println(part2(readAllText("local/day15_input.txt")))
 }
 
-fun part1(input: String) = input.lineSequence().filterNot(String::isBlank)
-    .count()
+fun part1(input: String): Long {
+    val ingredients = parse(input).toList()
+    return possibilities()
+        .map { counts ->
+            counts.zip(ingredients) { c, i -> i * c }.reduce { a, i -> a + i }
+        }
+        .map { total->
+            (total.a.coerceAtLeast(0) * total.b.coerceAtLeast(0) * total.c.coerceAtLeast(0) * total.d.coerceAtLeast(0))
+        }
+        .max()
+}
+fun part2(input: String): Long {
+    val ingredients = parse(input).toList()
+    return possibilities()
+        .map { counts ->
+            counts.zip(ingredients) { c, i -> i * c }.reduce { a, i -> a + i }
+        }
+        .filter { it.e == 500L }
+        .map {
+            it.a.coerceAtLeast(0) * it.b.coerceAtLeast(0) * it.c.coerceAtLeast(0) * it.d.coerceAtLeast(0)
+        }
+        .max()
+}
 
-fun part2(input: String) = input.lineSequence().filterNot(String::isBlank)
-    .count()
+private fun possibilities() = sequence {
+    (0..100L).forEach { i ->
+        (0..100 - i).forEach { j ->
+            (0..100 - i - j).forEach { k ->
+                yield(listOf(i, j, k, 100 - i - j - k))
+            }
+        }
+    }
+}
+
+data class Ingredients(val a: Long, val b: Long, val c: Long, val d: Long, val e: Long) {
+    operator fun times(i: Long) = Ingredients(a * i, b * i, c * i, d * i, e * i)
+    operator fun plus(o: Ingredients) = Ingredients(a + o.a, b + o.b, c + o.c, d + o.d, e + o.e)
+}
+
+private val regex =
+    "\\w+: capacity (-?\\d+), durability (-?\\d+), flavor (-?\\d+), texture (-?\\d+), calories (-?\\d+)".toRegex()
+
+private fun parse(input: String) = input.lineSequence().filterNot(String::isBlank)
+    .map { line -> regex.matchEntire(line)?.destructured ?: error("WTF `$line`") }
+    .map { (a, b, c, d, e) -> Ingredients(a.toLong(), b.toLong(), c.toLong(), d.toLong(), e.toLong()) }
